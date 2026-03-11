accb1284d154627d96f1327c1bc92a9ec8bf7860
TrialNTerror <99099246+TrialNTerror@users.noreply.github.com>
2026-03-11 17:10:07 -0500

    Adding CRT

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -30,6 +30,9 @@ import com.revrobotics.PersistMode;
 
 import frc.robot.Constants.TurretConstants;
 
+import yams.units.EasyCRT;
+import yams.units.EasyCRTConfig;
+
 public class Turret extends SubsystemBase{
     private SparkMaxConfig turretMotorConfig;
     private SparkMaxConfig followerMotorConfig;
@@ -127,7 +130,56 @@ public class Turret extends SubsystemBase{
     public void periodic()
     {
         SmartDashboard.putNumber("Flywheel Speed: ", TurretConstants.FlywheelSpeed + FlywheelAdjust);
+        if(turretSolver(getAbsoluteEncoderAngleSupplier(), kicker.getAbsoluteEncoderAngleSupplier()).getAngleOptional().isPresent()){
+            turretSolver(getAbsoluteEncoderAngleSupplier(), kicker.getAbsoluteEncoderAngleSupplier()).getAngleOptional().ifPresent(turretAngle -> { SmartDashboard.putNumber("Turret Angle Pos", turretAngle.in(Rotations));});
+        }
     }
+
+    public Supplier<Angle> getAbsoluteEncoderAngleSupplier(){
+
+        Angle encoderPosition = Rotations.of(m_rotationTurret.getAbsoluteEncoder().getPosition());
+
+    return () -> encoderPosition;
+
+  }
+
+  /**
+   * Calculates a target pose relative to an AprilTag on the field.
+   *
+   * @param encoder1Supplier    The First Encoder
+   * @param encoder2Supplier    The Second Encoder
+   * 
+   * @return The Easy CRT solver.
+   */
+  public EasyCRT turretSolver(Supplier<Angle> encoder1Supplier, Supplier<Angle> encoder2Supplier)
+  {
+    // Suppose: mechanism : drive gear = 12:1, drive gear = 50T, encoders use 19T and 23T pinions.
+    var easyCrt = new EasyCRTConfig(encoder1Supplier, encoder2Supplier)
+            .withCommonDriveGear(
+                /* commonRatio (mech:drive) */ 9.52380952,
+                /* driveGearTeeth */ 21,
+                /* encoder1Pinion */ 19,
+                /* encoder2Pinion */ 21)
+            .withAbsoluteEncoderOffsets(Rotations.of(0.0), Rotations.of(0.0)) // set after mechanical zero
+            .withMechanismRange(Rotations.of(-1.0), Rotations.of(1.0)) // -360 deg to +720 deg
+            .withMatchTolerance(Rotations.of(0.06)) // ~1.08 deg at encoder2 for the example ratio
+            .withAbsoluteEncoderInversions(false, false)
+            .withCrtGearRecommendationConstraints(
+                /* coverageMargin */ 1.2,
+                /* minTeeth */ 15,
+                /* maxTeeth */ 45,
+                /* maxIterations */ 30);
+
+    // you can inspect:
+    easyCrt.getUniqueCoverage();          // Optional<Angle> coverage from prime counts and common scale
+    easyCrt.coverageSatisfiesRange();     // Does coverage exceed maxMechanismAngle?
+    easyCrt.getRecommendedCrtGearPair();  // Suggested pair within constraints
+
+    // Create the solver:
+    var easyCrtSolver = new EasyCRT(easyCrt);
+
+    return easyCrtSolver;
+  }
     
     public Command adjustFlywheelSpeed(double isInverted){
     return this.runOnce(() -> FlywheelAdjust += (isInverted * TurretConstants.FlywheelAdjust));
